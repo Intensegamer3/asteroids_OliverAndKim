9997394f5d3988b96ec32c194c0fdabfcb654acb
Intensegamer3 <120102125+Intensegamer3@users.noreply.github.com>
2026-05-17 17:43:25 +0200

    Prepared Project

---
+++ HelloWorld/MainGame.cpp
@@ -0,0 +1,24 @@
+#include "GameManager.h"
+
+// The entry point for a PlayBuffer program
+void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
+{
+	Play::CreateManager( DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE );
+}
+
+// Called by PlayBuffer every frame (60 times a second!)
+bool MainGameUpdate( float elapsedTime )
+{
+	Play::ClearDrawingBuffer( Play::cOrange );
+	Play::DrawDebugText( { DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 }, "Hello World!" );
+	Play::PresentDrawingBuffer();
+	return Play::KeyDown( KEY_ESCAPE );
+}
+
+// Gets called once when the player quits the game 
+int MainGameExit( void )
+{
+	Play::DestroyManager();
+	return PLAY_OK;
+}
+
